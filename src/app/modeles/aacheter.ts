b6b3cf230a2e8a9b311c7fd1a3541a5bfbd26f83
clement <clement.journouxlapp@gmail.com>
2019-09-17 15:00:25 +0200

    message

--- src/app/modeles/aacheter.ts
+++ src/app/modeles/aacheter.ts
@@ -1,2 +1,11 @@
 export class AAcheter {
+
+    
+   public idBienImmobilier:number;
+   public statutBienImmobilier:string;
+   public dateSoumissionBienImmobilier:string;
+   public localisationBienImmobilier:string;
+   public photo:any;
+   public prixDemandeAAcheter:number;
+   public etatAAcheter:string;
 }
--- src/app/modeles/alouer.ts
+++ src/app/modeles/alouer.ts
@@ -1,2 +1,13 @@
 export class ALouer {
+
+   public idBienImmobilier:number;
+   public statutBienImmobilier:string;
+   public dateSoumissionBienImmobilier:string;
+   public localisationBienImmobilier:string;
+   public photo:any;
+   public cautionALouer:number;
+   public loyerAlouer:number;
+   public chargesALouer:number;
+   public bailALouer:string;
+   public garnituresALouer:string;
 }
