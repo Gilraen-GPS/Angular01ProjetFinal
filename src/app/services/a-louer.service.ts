123d9798ff512f16591d7c4a7355978d93a604d8
clement <clement.journouxlapp@gmail.com>
2019-09-17 15:13:59 +0200

    p

--- src/app/services/a-louer.service.ts
+++ src/app/services/a-louer.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ALouer } from '../modeles/alouer';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ALouerService {
+  url : string = "http://localhost:8082/01_Agence_Immobiliere/rest/contrat/";
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  addALouer(aLouer : ALouer){
+      return this.httpClient.post<ALouer>(this.url+"add", aLouer);
+  }
+
+  updateALouer(aLouer : ALouer){
+      return this.httpClient.put<ALouer>(this.url+"update", aLouer, {observe : 'response'});
+  }
+
+  deleteALouer(idVisite : number){
+      return this.httpClient.delete(this.url+"delete/"+idVisite, {observe : 'response'});
+  }
+
+  getAllALouer(){
+      return this.httpClient.get(this.url+"getAll");
+  }
+
+  getByIdALouer(idVisite : number){
+      return this.httpClient.get<ALouer>(this.url+"getById/"+idVisite);
+  }
 }
--- src/app/services/visite.service.ts
+++ src/app/services/visite.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Visite } from '../modeles/visite';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisiteService {
 
-  constructor() { }
+  url : string = "http://localhost:8082/01_Agence_Immobiliere/rest/contrat/";
+
+  constructor(private httpClient:HttpClient) { }
+
+  addVisite(visite : Visite){
+      return this.httpClient.post<Visite>(this.url+"add", visite);
+  }
+
+  updateVisite(visite : Visite){
+      return this.httpClient.put<Visite>(this.url+"update", visite, {observe : 'response'});
+  }
+
+  deleteVisite(idVisite : number){
+      return this.httpClient.delete(this.url+"delete/"+idVisite, {observe : 'response'});
+  }
+
+  getAllVisite(){
+      return this.httpClient.get(this.url+"getAll");
+  }
+
+  getByIdVisite(idVisite : number){
+      return this.httpClient.get<Visite>(this.url+"getById/"+idVisite);
+  }
 }
