e2fb89dd1b49845582d1c7d82caf780f39508801
Wasselin Romain <54113275+wattt8wattt@users.noreply.github.com>
2019-09-18 15:45:26 +0200

    h

--- src/app/components/aAcheter/delete-aacheter/delete-aacheter.component.ts
+++ src/app/components/aAcheter/delete-aacheter/delete-aacheter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AAcheterService } from 'src/app/services/a-acheter.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-delete-aacheter',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteAAcheterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private aAcheterService : AAcheterService, private router : Router) { }
+  idBienImmobilier : number;
 
   ngOnInit() {
+  }
+
+  supprimer(){
+    this.aAcheterService.deleteCAAcheter(this.idBienImmobilier).subscribe(response=>{
+      if(response.status===200){
+        this.router.navigate(['/aAcheter/getAll']);
+      }
+    });
+
   }
 
 }
