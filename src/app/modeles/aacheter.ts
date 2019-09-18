a93ff393ed286fbf81144f82c3dd0501227f4e9a
Wasselin Romain <54113275+wattt8wattt@users.noreply.github.com>
2019-09-18 11:13:58 +0200

    aAcheter

--- src/app/modeles/aacheter.ts
+++ src/app/modeles/aacheter.ts
@@ -3,7 +3,14 @@ export class AAcheter {
    public statutBienImmobilier:string;
    public dateSoumissionBienImmobilier:string;
    public localisationBienImmobilier:string;
+   public dateDispoBienImmobilier : string ;
+   public revenuCadastralBienImmobilier : string;
+   public paysBienImmobilier : string;
    public photo:any;
+   public numeroFactureBienImmobilier:string;
+   public dateFactureBienImmobilier:string;
+   public numeroAffaireBienImmobilier:string;
+   public typeDeBienImmobilier:string;
    public prixDemandeAAcheter:number;
    public etatAAcheter:string;
 }
